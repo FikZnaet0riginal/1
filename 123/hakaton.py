8bc4cc0289c09c4502dc7bb6b4854a5008a17986
FikZnaet0riginal <140433155+FikZnaet0riginal@users.noreply.github.com>
2023-07-24 22:57:44 +0600

    Add files via upload

---
+++ 123/hakaton.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import sys
+
+# Дополнительные импорты (если потребуется):
+# import os
+# import time
+
+pygame.init()
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Чат-бот Подводный Мир")
